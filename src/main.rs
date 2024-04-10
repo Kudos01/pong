086d36a7a45386009bbc42e46a88c557c94e52fa
Felipe Perez <fpstoppa@gmail.com>
2024-04-11 00:01:27 +0200

    Drawing players and ball

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
 use macroquad::prelude::*;
+type Point = (f32, f32);
 
-#[macroquad::main("BasicShapes")]
+const RECTANGLE_WIDTH: f32 = 20.;
+
+const RECTANGLE_HEIGHT: f32 = 100.;
+
+const OFFSET: f32 = 20.;
+
+const CUBE_SIDE: f32 = 10.;
+
+#[derive(Debug, Clone, Copy)]
+struct Player {
+    pos: Point,
+    score: i16,
+}
+
+struct Ball {
+    pos: Point
+}
+
+fn draw_player(p: Player){
+    draw_rectangle(p.pos.0, p.pos.1, RECTANGLE_WIDTH, RECTANGLE_HEIGHT, BLACK);
+}
+
+#[macroquad::main("Pong")]
 async fn main() {
+
+    // Center the players at first
+
+    let mut p1: Player = Player { pos: (OFFSET, screen_height()/2. - RECTANGLE_HEIGHT + OFFSET), score: (0) };
+    let mut p2: Player = Player { pos: (screen_width() - OFFSET*2., screen_height()/2. - RECTANGLE_HEIGHT + OFFSET), score: (0)};
+
+    let mut ball: Ball = Ball { pos: (screen_width()/2., screen_height()/2.) };
+
     loop {
-        clear_background(RED);
+        clear_background(GRAY);
+        
+        draw_player(p1);
+        draw_player(p2);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
-        draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+        draw_rectangle(ball.pos.0, ball.pos.1, CUBE_SIDE, CUBE_SIDE, GOLD);
+        
+        // TODO Player movement
 
-        next_frame().await
+        // TODO Ball movement
+
+        // TODO Collisions
+
+        next_frame().await;
     }
 }
